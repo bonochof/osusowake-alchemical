ef1bcdf8c9c3638aed2dccf4a2180da1e8febd06
bonochof <ryos427@yahoo.co.jp>
2018-12-01 01:23:30 +0900

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_06_161504) do
+ActiveRecord::Schema.define(version: 2018_11_30_162244) do
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_10_06_161504) do
     t.integer "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
