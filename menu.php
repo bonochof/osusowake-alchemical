b7a3fce252247b7160dee936f9835a9ead7e0e8c
cs16094 <mukkunn208w@gmail.com>
2018-12-15 09:51:30 +0900

    menu search proto

--- menu.php
+++ menu.php
@@ -75,13 +75,29 @@ try {
   case "DBdelete": //メニュー削除完了ページ
     $del_id = $_POST["menu_id"];
     if (preg_match("/^[0-9]+$/", $del_id)) {
-      $s->query("delete from osusowake where id=$del_id");
+      $s->query("delete from menu where id=$del_id");
     }
-    $re = $s->query("select * from osusowake order by menu_id");
+    $re = $s->query("select * from menu order by id");
     break;
   case "search": //メニュー検索ページ
-    break;
-  case "DBsearch": // メニュー検索完了ページ
+    $ser_word = $_POST["search_word"];
+    $ser_type = $_POST["search_type"];
+   /* $ser_word = mb_convert_kana($ser_word, 's', 'utf-8');*/
+    $words = preg_split('/[\s]+/', $ser_word, -1, PREG_SPLIT_NO_EMPTY);
+    $query = "select * from menu where (name like '%$words[0]%' or ing like '%$words[0]%' or amount like '%$words[0]%' or author like '%$words[0]%' or date like '%$words[0]%') ";
+    for ($i = 1; $i < count($words); $i++) {
+      if($ser_type == 'and'){
+        $query .= "and ";
+      }
+      else{
+        $query .= "or ";
+      }
+      $query .= "(name like '%$words[$i]%' or ing like '%$words[$i]%' or amount like '%$words[$i]%' or author like '%$words[$i]%' or date like '%$words[$i]%') ";
+    }
+    $query .= "order by id";
+    $result = $s->query($query);
+   // var_dump("$query");
+    var_dump($result->fetchAll());
     break;
   default:         // メニュー表示ページ
     echo "<p><form method='POST' action='menu.php'>";
@@ -99,10 +115,13 @@ try {
       echo "<div class='textbox'><input type='image' value='ins' class='insert' align='right'></div>";
       echo "<input type='hidden' name='h' value='insert'>";
       echo "</form></p>";
-     /* echo "<p><form method='POST' action='menu.php'>";
-      echo "<div class='textbox'><input type='image' value='ser' class='search' align='right'></div>";
+      echo "<p><form method='POST' action='menu.php'>";
+      echo "<input type='text' name='search_word'>";
+      echo "<input type='radio' name='search_type' value='and' checked='checked'>";
+      echo "<input type='radio' name='search_type' value='or'>";
+      echo "<input type ='submit' value = 'search'>";
       echo "<input type='hidden' name='h' value='search'>";
-      echo "</form></p>";*/
+      echo "</form></p>";
     }
     echo "</div>";
     break;
